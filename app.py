ad4451d38d2a1bd138ffe0386c4f1315af73af5c
Ninnnn <30983961+Ninnnn@users.noreply.github.com>
2025-04-20 19:58:39 +0800

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+import os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+# 初始化 Flask
+app = Flask(__name__)
+
+# 初始化 LINE Bot API 與 handler
+line_bot_api = LineBotApi(os.environ.get("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("LINE_CHANNEL_SECRET"))
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_id = event.source.user_id
